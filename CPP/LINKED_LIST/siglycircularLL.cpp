6e997440a97e7b5f7fc02b503dd9f884a244960c
Suman Kumari <sumankumari200603@gmail.com>
2026-02-04 10:19:15 +0530

    save

--- CPP/LINKED_LIST/siglycircularLL.cpp
+++ CPP/LINKED_LIST/siglycircularLL.cpp
@@ -38,6 +38,42 @@ void insertatbeg(Node* &head,int val){
 	node->next=head;
 	head=node;
 }
+void deleteatbeg(Node* &head){
+	if(head==NULL){
+		
+		return;
+	}
+//	if(head->next=head){
+//		delete head;
+//		head=NULL;
+//		return;
+//	}
+	Node* temp=head;
+	while(temp->next!=head){
+		temp=temp->next;
+	}
+	Node* todel=head;
+	head=head->next;
+	temp->next=head;
+	delete todel;
+}
+void deleteatend(Node* &head){
+	if(head==NULL){
+		return;
+	}
+	Node* temp=head;
+	while(temp->next->next!=head){
+		temp=temp->next;
+	}
+	if(head=head->next){
+		delete head;
+		head=NULL;
+		return;
+	}
+	Node* todel=temp->next;
+	temp->next=head;
+	delete(todel);
+}
 void printLL(Node* head){
 	Node* temp=head;
 	
@@ -58,5 +94,10 @@ int main(){
 	Node* head=NULL;
 	insertatbeg(head,1);
 	insertatend(head,2);
+	
 	printLL(head);
+//	deleteatend(head);
+deleteatbeg(head);
+	cout<<endl;
+		printLL(head);
 }
