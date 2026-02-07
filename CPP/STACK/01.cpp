29ae6730fd512448b5a125fe5129a2c00106f556
Suman Kumari <sumankumari200603@gmail.com>
2026-02-07 19:21:59 +0530

    save

---
+++ CPP/STACK/01.cpp
@@ -0,0 +1,11 @@
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+//     stack<int> st;
+//     st.push(1);
+//     st.push(2);
+//     st.push(3);
+//     cout<<"top element is: "<<st.top()<<endl;
+//     st.pop();
+// }
