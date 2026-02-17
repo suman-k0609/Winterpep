e89f226caa5f12d6ec02dc64519c7c0d2f061833
Suman Kumari <sumankumari200603@gmail.com>
2026-02-17 16:13:50 +0530

    save

---
+++ DSA/BST/basic.cpp
@@ -0,0 +1,7 @@
+//Binary search tree is a special type of tree where every node has at most two children
+//for every node-
+//All values in the LST are smaller
+//All values in RST are greater
+//# Both LST AND BST are also BST
+
+//the inorder tranversal(left root right) of BST is always sorted
