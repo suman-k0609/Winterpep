c6d9645ee3f97d93f2169099f3a6ac1291e3cfb7
Suman Kumari <sumankumari200603@gmail.com>
2026-03-05 22:19:45 +0530

    save

---
+++ DEVOPS/1st.cpp
@@ -0,0 +1,24 @@
+
+
+// *Windows Tools*
+
+// install chocolatey from the instructions given in the link below
+
+// https://chocolatey.org/install
+
+// choco install virtualbox --version=7.1.4 -y
+
+// choco install vagrant --version=2.4.3 -y
+
+// choco install git -y
+
+// choco install corretto17jdk -y
+
+// choco install maven -y
+
+// choco install awscli -y
+
+// choco install intellijidea-community -y
+
+
+// choco install vscode -y
