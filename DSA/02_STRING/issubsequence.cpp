cceb39167d88c1c024300e175515aac70bf417fa
Suman Kumari <sumankumari200603@gmail.com>
2026-03-11 23:18:42 +0530

    save

---
+++ DSA/02_STRING/issubsequence.cpp
@@ -0,0 +1,15 @@
+// bool isSubsequence(string s, string t) {
+//     int i = 0, j = 0;
+
+//     while (i < s.length() && j < t.length()) {
+//         if (s[i] == t[j]) {
+//             i++;  // move pointer in s
+//         }
+//         j++;      // always move pointer in t
+//     }
+
+//     if (i == s.length())
+//         return true;
+//     else
+//         return false;
+// }
