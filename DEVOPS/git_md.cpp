082a9610b95bb9ece3cf166668fc014dcb5a6214
Suman Kumari <sumankumari200603@gmail.com>
2026-03-23 23:39:07 +0530

    save

--- DEVOPS/git_md.cpp
+++ DEVOPS/git_md.cpp
@@ -21,3 +21,19 @@
 
 // git log --grep="first" 
 //kj
+
+// git init -> initialize git and creates the .git/ folder git status -> check the current status -> git's dashboard
+
+// Working ----> staging Directory git add area
+
+// git add -> stage the file for which we give name git add . -> stage all the files
+
+// git commit -m "Write a message" -> commit with a message
+
+// git log -> full log git log --oneline -> compact one line format git log --oneline --graph --all
+
+// git log -5 --oneline -> shows last 5 commits
+
+// git log --author="adhirajChauhan"
+
+// git log --grep="first"
